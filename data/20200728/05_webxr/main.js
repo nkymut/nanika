9b9b3c24ddcc083274f47f7f978bc679c8aa5829
Yuta Nakayama <nkymut@gmail.com>
2020-07-28 23:09:43 +0800

    mesh resize

--- data/20200728/05_webxr/main.js
+++ data/20200728/05_webxr/main.js
@@ -19,36 +19,36 @@ function init() {
 
     document.body.appendChild(renderer.domElement);
     //var geometry = new THREE.BoxGeometry();
-    var geometry = new THREE.BoxBufferGeometry(32, 25, 92);
+    var geometry = new THREE.BoxBufferGeometry(3.2, 2.5, 9.2);
     // var material = new THREE.MeshBasicMaterial( { color: 0x00ffff } );
     var loader = new THREE.TextureLoader();
     // const texture = new THREE.TextureLoader().load('img/AXE_Texture.png');
     var sphere = new THREE.SphereBufferGeometry(0.5, 16, 8);
 
-    let ambientLight = new THREE.AmbientLight(0xffffff, 0.03);
+    let ambientLight = new THREE.AmbientLight(0xffffff, 0.3);
     scene.add(ambientLight);
 
-    //light = new THREE.PointLight(0xff0040, 1, 0, 5);
-    light = new THREE.PointLight(0xffffff, 1, 200,2);
-    light.add(new THREE.Mesh(sphere, new THREE.MeshBasicMaterial({ color: 0xff0040 })));
-    light.castShadow = true;
-    light.shadow.camera.near = 0.1;
-    light.shadow.camera.far = 1000;
-    light.position.set(20, 20, 20);
-    light.shadowCameraVisible = true;
-    //light1 = new THREE.PointLight(0x4000ff, 1, 200, 2);
-    light1 = new THREE.PointLight(0xffffff, 1, 200,2);
-    light1.add(new THREE.Mesh(sphere, new THREE.MeshBasicMaterial({ color: 0x4000ff })));
-    light1.castShadow = true;
-    light1.shadow.camera.near = 0.1;
-    light1.shadow.camera.far = 1000;
-    light1.position.set(20, 20, -20);
+    // //light = new THREE.PointLight(0xff0040, 1, 0, 5);
+    // light = new THREE.PointLight(0xffffff, 1, 200,2);
+    // light.add(new THREE.Mesh(sphere, new THREE.MeshBasicMaterial({ color: 0xff0040 })));
+    // light.castShadow = true;
+    // light.shadow.camera.near = 0.1;
+    // light.shadow.camera.far = 1000;
+    // light.position.set(20, 20, 20);
+    // light.shadowCameraVisible = true;
+    // //light1 = new THREE.PointLight(0x4000ff, 1, 200, 2);
+    // light1 = new THREE.PointLight(0xffffff, 1, 200,2);
+    // light1.add(new THREE.Mesh(sphere, new THREE.MeshBasicMaterial({ color: 0x4000ff })));
+    // light1.castShadow = true;
+    // light1.shadow.camera.near = 0.1;
+    // light1.shadow.camera.far = 1000;
+    // light1.position.set(20, 20, -20);
 
-    const controls = new THREE.OrbitControls(camera, renderer.domElement);
-    controls.minZoom = 1;
-    controls.maxZoom = 1.5;
+    // const controls = new THREE.OrbitControls(camera, renderer.domElement);
+    // controls.minZoom = 1;
+    // controls.maxZoom = 1.5;
 
-    controls.enablePan = false;
+    // controls.enablePan = false;
 
     function onSelect() {
         console.log("onSelect");
@@ -64,11 +64,11 @@ function init() {
     controller.addEventListener('select', onSelect);
     scene.add(controller);
 
-    meshFloor = new THREE.Mesh(new THREE.PlaneGeometry(100, 100, 100, 100), new THREE.MeshPhongMaterial({ color: 0xffffff }));
-    meshFloor.material.side = THREE.DoubleSide;
-    meshFloor.receiveShadow = true;
-    //meshFloor.castShadow = true;
-    scene.add(meshFloor);
+    // meshFloor = new THREE.Mesh(new THREE.PlaneGeometry(100, 100, 100, 100), new THREE.MeshPhongMaterial({ color: 0xffffff }));
+    // meshFloor.material.side = THREE.DoubleSide;
+    // meshFloor.receiveShadow = true;
+    // //meshFloor.castShadow = true;
+    // scene.add(meshFloor);
 
     // const textureCube = loader.load('img/1.png', 'img/2.png',
     //                                 'img/3.png', 'img/4.png',
